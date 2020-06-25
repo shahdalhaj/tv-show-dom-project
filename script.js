0a25ed3aafd3da79f9bcf8c0e9d03f85f561fedd
Shahd Alhaj <shahdadil2012@hotmaail.com>
2020-06-25 21:53:04 +0100

    styling

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function setup() {
 }
 const rootElem = document.getElementById("root");
 function makePageForEpisodes(episodeList) {
+
+const display = document.getElementById("display");
+display.innerText = " Display: 0/73";
+
  
  /* rootElem.textContent = `Got ${episodeList.length} episode(s)`; */
  
@@ -12,11 +16,11 @@ episodeList.forEach(e => {
   const boxBody = document.createElement('div');
   boxBody.style.display ="flex";
   boxBody.style.flexDirection = "column";
-  boxBody.style.backgroundColor = "#e8c4f8";
+  boxBody.style.backgroundColor = "black";
   boxBody.style.margin ="1.5em";
   boxBody.style.width ="250px";
   boxBody.style.height ="330px";
-  boxBody.style.border ="1px solid #7fe672";
+  boxBody.style.border ="3px solid black";
   boxBody.style.borderRadius = "9px";
   const title = document.createElement('h3');
   title.style.textAlign = "center";
@@ -31,7 +35,8 @@ episodeList.forEach(e => {
   boxBody.appendChild(title);
   boxBody.appendChild(imgBox);
   boxBody.appendChild(para);
-  title.innerHTML = e.name +" S"+ "0"+e.season+"E0"+e.number;
+  title.innerHTML = `${e.name}`
+  +  `-S${e.season.toString().padStart(2,'0')}E${e.number.toString().padStart(2,'0')}`;
   imgBox.src = e.image.medium;
   para.innerHTML = e.summary;
   para.style.fontFamily = " Georgia, 'Times New Roman', Times, serif";
@@ -43,5 +48,18 @@ episodeList.forEach(e => {
  /*  console.log(rootElem) */
 });
 }
+//setting the filterd search results 
+const myInput = document.getElementById("search");
+//const myList = document.getElementsById("list");
+
+
+
+
+  myInput.addEventListener('keyup', function(){
+    const result = this.value.toLowerCase();
+    console.log(result)
+    });
+ 
+  
 
 window.onload = setup;
