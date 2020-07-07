993060b9332e67142c7db8d2361888910bdeb896
Shahd Alhaj <shahdadil2012@hotmaail.com>
2020-07-07 15:39:23 +0100

    almost 400

--- script.js
+++ script.js
@@ -5,29 +5,55 @@ const allEpisodes = getAllEpisodes();
 const searchBar = document.getElementById("search");
 const display = document.getElementById("display");
 const btn = document.getElementById("btn")
-//const allShows = getAllShows();
+const allShows = getAllShows();
+const showDropDown = document.getElementById("showList")
+let initialId = "82";
 let shows = [];
 
+let showId = allShows.map(el=> el.id);
+//console.log(showId)
 
-function ShowDisplayer(){
-  fetch(`https://api.tvmaze.com/shows/82/episodes`)
+ function showDisplayer(showId){
+  fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
   .then(response => response.json())
   .then((data) =>{ shows = [...data] 
   makePageForEpisodes(data)
-  displayer(data)
-  episodeFilter(data)
-  dropDownSelection(shows)})
+  dropDownSelection(data)})
   .catch((error) => console.log(error));
-  
-}
+} 
 
-function setup() {
-  ShowDisplayer()
-  makePageForEpisodes(allEpisodes);
-  displayer()
-  episodesFilter()
-  dropDownSelection()
+function makeListForEchEpi(episodes) {
+  episodes.forEach((episode) => dropDownSelection(episode));
 }
+ //sorting based on alphabetically before display on select list
+const sorted = allShows.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); 
+  var nameB = b.name.toUpperCase(); 
+  if (nameA < nameB) {
+    return -1; 
+  }
+  if (nameA > nameB) {
+    return 1; 
+  }
+  return 0;  
+});
+
+
+console.log(allShows)
+// Display shows Dropdown
+function showsDropDownMenu() {
+   for (let x = 0; x < sorted.length; x++) {
+    let showOptions = document.createElement('option');
+    showOptions.innerText =`${sorted[x].name}`;
+    showDropDown.appendChild(showOptions)
+   }
+   }
+
+
+
+
+
+
 function shower() {
   makePageForEpisodes(allEpisodes)
 }
@@ -35,7 +61,7 @@ function shower() {
 
 function makePageForEpisodes(episodeList) {
   rootElem.innerHTML = "";
-  display.innerText = `Display: ${episodeList.length}/${allEpisodes.length}`;
+  display.innerText = `Display: ${episodeList.length}/${allShows.length}`;
   episodeList.forEach(e => {
   const boxBody = document.createElement('div');
   boxBody.id = "boxBody";
@@ -47,12 +73,13 @@ function makePageForEpisodes(episodeList) {
   para.id = "para"
   rootElem.appendChild(boxBody);
   boxBody.append(title,imgBox,para);
-  title.innerHTML = `${e.name}`
+  title.innerText = `${e.name}`
   +`-S${e.season.toString().padStart(2,'0')}E${e.number.toString().padStart(2,'0')}`;
   imgBox.src = e.image.medium;
   para.innerHTML = e.summary;
 });
 }
+
 
 //setting the filterd search results 
 function episodesFilter() {
@@ -73,7 +100,7 @@ function episodesFilter() {
  function displayer() {
   allEpisodes.forEach(element => {
     let options = document.createElement('option');
-    options.innerHTML = `S${element.season.toString().padStart(2,'0')}E${element.number.toString().padStart(2,'0')}-${element.name}`;
+    options.innerText = `S${element.season.toString().padStart(2,'0')}E${element.number.toString().padStart(2,'0')}-${element.name}`;
     dropDown.appendChild(options)
   });
  }
@@ -94,6 +121,36 @@ if (selector === "Select an Episode") {
   return makePageForEpisodes(chosenEpi)
 }
 }); 
+}
+
+//show Selector based on the show id
+function showDropDownSelection() {
+  showDropDown.addEventListener("change",function(o){
+    let selector2 = o.target.value;
+    console.log(selector2)
+    let chosenShow = sorted.filter((find)=>{
+      find = find.name ;
+      return selector2 === find;
+    })
+    if (selector2 === "Show Selector") {
+      return showDisplayer(initialId)
+    } else {
+      return showDisplayer(chosenShow[0].id)
+    }
+  })
+}
+ function makeListForEachShow(episode) {
+   
+ }
+
+function setup() {
+  showsDropDownMenu()
+  showDisplayer(initialId)
+  makePageForEpisodes(allEpisodes);
+  displayer()
+  episodesFilter()
+  dropDownSelection()
+  showDropDownSelection()
 }
 
 window.onload = setup;
